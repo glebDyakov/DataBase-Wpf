7e321944f70f2c4436a47acb6013d1bf0e11a698
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-01-09 16:38:46 +0300

    finish user form

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace UsersApp
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+
+            /*else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно";
+                passBox_2.Background = Brushes.DarkRed;
+            }*/
+            else if (email.Length < 5 || !email.Contains("@)") || !email.Contains('.'))
+            {
+
+                textBoxEmail.ToolTip = "Это поле введено не корректно";
+                textBoxEmail.Background = Brushes.DarkRed;
+
+            }
+            else {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+                MessageBox.Show("всё хорошо!");
+            }
+        }
     }
 }
